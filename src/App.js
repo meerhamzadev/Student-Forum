b6a305de328c978192769716fb7784504fadb25a
MeerHamza1421 <hamzababar37@gmail.com>
2021-04-25 11:42:53 -0700

    Add welcome page of the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage/HomePage';
+import WelcomePage from './Components/WelcomePage/WelcomePage';
 
 function App() {
   return (
     <>
+      {/* <WelcomePage /> */}
       <HomePage />
     </>
   );
--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,24 +1,33 @@
-import {Button, IconButton} from '@material-ui/core'
-import {AccountCircle, MenuRounded} from '@material-ui/icons'
+import { Button, IconButton } from '@material-ui/core'
+import { AccountCircle, MenuRounded } from '@material-ui/icons'
 import React from 'react'
 
-function Navbar() {
+const Navbar = ({ isLoggedIn }) => {
     return (
         <nav className="navbar">
             <IconButton className="navbar__logo">LOGO</IconButton>
-            
+
             <div className="navbar__buttons">
                 <Button variant="extendedFab" className="navbar__buttons__button">Home</Button>
                 <Button variant="extendedFab" className="navbar__buttons__button">Books</Button>
                 <Button variant="extendedFab" className="navbar__buttons__button">Past Papers</Button>
                 <Button variant="extendedFab" className="navbar__buttons__button">Exams</Button>
                 <Button variant="extendedFab" className="navbar__buttons__button">Queries</Button>
-                <IconButton>
-                    <MenuRounded className="navbar__buttons__menu" />
-                </IconButton>
-                <IconButton>
-                    <AccountCircle className="navbar__buttons__account" />
-                </IconButton>
+                {
+                    isLoggedIn ?
+
+                        <>
+                            <IconButton>
+                                <MenuRounded className="navbar__buttons__menu" />
+                            </IconButton>
+                            <IconButton>
+                                <AccountCircle className="navbar__buttons__account" />
+                            </IconButton>
+                        </>
+
+                        :
+                        <button className="navbar__buttons__loginButton" >Login</button>
+                }
             </div>
         </nav>
     )
