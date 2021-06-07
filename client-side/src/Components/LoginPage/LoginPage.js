855fe02cf8c779167533273f203a606c672082bc
HaiderSultanArc <haidersultanarc@gmail.com>
2021-06-08 00:42:07 +0500

    Pages linked and Login Register made responsive

--- client-side/src/Components/LoginPage/LoginPage.js
+++ client-side/src/Components/LoginPage/LoginPage.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
-import { AccountCircle } from '@material-ui/icons'
+import {Button} from '@material-ui/core';
+import {AccountCircle} from '@material-ui/icons';
+import React from 'react';
 
 function LoginPage() {
+      const goBack = () => {
+            window.history.back();
+      }
+      
       return (
-            <div className="login__page">
-                  <AccountCircle className="login__page__avatar" />
-                  <form className="login__page__form">
-                        <input type="text" className="login__page__form__input" placeholder="Enter the Email " />
-                        <input type="Password" className="login__page__form__input" placeholder="Enter the Password" />
-                        <Button className="login__page__form__button" >Login</Button>
-                  </form>
-
-
+            <div className="login">
+                  <div className="login__page">
+                        <AccountCircle className="login__page__avatar" />
+                        <form className="login__page__form">
+                              <input type="text" className="login__page__form__input" placeholder="Enter the Email " />
+                              <input type="Password" className="login__page__form__input" placeholder="Enter the Password" />
+                              <div className="login__page__form__buttons">
+                                    <Button className="login__page__form__buttons__button" onClick={goBack} >Back</Button>
+                                    <Button className="login__page__form__buttons__button" >Login</Button>
+                              </div>
+                              <div className="login__page__form__registerQuery">
+                                    <p>Don't have an account? <a href="/register">Create Account</a></p>
+                              </div>
+                        </form>
+                  </div>
             </div>
-
       )
 }
 
--- client-side/src/Components/RegistrationPage/RegistrationPage.js
+++ client-side/src/Components/RegistrationPage/RegistrationPage.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
-import { AccountCircle } from '@material-ui/icons'
+import {Button} from '@material-ui/core';
+import {AccountCircle} from '@material-ui/icons';
+import React from 'react';
 
 function RegistrationPage() {
+    const goBack = () => {
+        window.history.back();
+    }
+    
     return (
-        <div className="reg__page">
-            <AccountCircle className="reg__page__avatar" />
-            <form className="reg__page__form">
-                <input className="reg__page__form__input" type="text" placeholder="Enter the Name" />
-                <input className="reg__page__form__input" type="text" placeholder="Enter the Email" />
-                <input className="reg__page__form__input" type="password" placeholder="Type the Password" />
-                <input className="reg__page__form__input" type="password" placeholder="Retype Password to Confirm" />
+        <div className="reg">
+            <div className="reg__page">
+                <AccountCircle className="reg__page__avatar" />
+                <form className="reg__page__form">
+                    <input className="reg__page__form__input" type="text" placeholder="Enter the Name" />
+                    <input className="reg__page__form__input" type="text" placeholder="Enter the Email" />
+                    <input className="reg__page__form__input" type="password" placeholder="Type the Password" />
+                    <input className="reg__page__form__input" type="password" placeholder="Retype Password to Confirm" />
 
-                <div className="reg__page__form__buttons">
-                    <Button className="reg__page__form__buttons__button" >Back</Button>
-                    <Button className="reg__page__form__buttons__button" >Register</Button>
-
-                </div>
-            </form>
+                    <div className="reg__page__form__buttons">
+                        <Button className="reg__page__form__buttons__button" onClick={goBack}>Back</Button>
+                        <Button className="reg__page__form__buttons__button">Register</Button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
