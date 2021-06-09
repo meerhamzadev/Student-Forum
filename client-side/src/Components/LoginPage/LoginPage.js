6907c927c0714f99549bc5e034b157b36a2147ef
MeerHamza1421 <hamzababar37@gmail.com>
2021-06-09 06:36:27 -0700

    📦 NEW: Connect login & reg pages to its backend

--- client-side/src/Components/LoginPage/LoginPage.js
+++ client-side/src/Components/LoginPage/LoginPage.js
@@ -1,27 +1,56 @@
-import {Button} from '@material-ui/core';
-import {AccountCircle} from '@material-ui/icons';
+import { Button } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
 import React from 'react';
-
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 function LoginPage() {
       const goBack = () => {
             window.history.back();
       }
-      
+
+      const loggedIn = async (e) => {
+            e.preventDefault();
+            const { email, password } = e.target;
+
+            const Data = {
+                  email: email.value,
+                  password: password.value
+            }
+            try {
+
+                  const response = await axios.post('http://localhost:5000/login', Data);
+                  console.log(response);
+                  if (response.status === 200) {
+                        const switchLink = document.querySelector('.login__page__homePageLink');
+                        switchLink.click();
+                  }
+            }
+            catch (err) {
+                  if (err.message.indexOf('404') !== -1) {
+                        window.alert('Error 404: User not exist\nRegister first to Login')
+                  }
+                  else {
+                        window.alert('Error 500: Server error')
+                  }
+            }
+
+      }
       return (
             <div className="login">
                   <div className="login__page">
                         <AccountCircle className="login__page__avatar" />
-                        <form className="login__page__form">
-                              <input type="text" className="login__page__form__input" placeholder="Enter the Email " />
-                              <input type="Password" className="login__page__form__input" placeholder="Enter the Password" />
+                        <form onSubmit={loggedIn} className="login__page__form">
+                              <input name="email" type="text" className="login__page__form__input" placeholder="Enter the Email " />
+                              <input name="password" type="Password" className="login__page__form__input" placeholder="Enter the Password" />
                               <div className="login__page__form__buttons">
                                     <Button className="login__page__form__buttons__button" onClick={goBack} >Back</Button>
-                                    <Button className="login__page__form__buttons__button" >Login</Button>
+                                    <Button type="submit" className="login__page__form__buttons__button" >Login</Button>
                               </div>
                               <div className="login__page__form__registerQuery">
                                     <p>Don't have an account? <a href="/register">Create Account</a></p>
                               </div>
                         </form>
+                        <Link to="/home" className="login__page__homePageLink"> </Link>
                   </div>
             </div>
       )
--- client-side/src/Components/RegistrationPage/RegistrationPage.js
+++ client-side/src/Components/RegistrationPage/RegistrationPage.js
@@ -1,27 +1,50 @@
-import {Button} from '@material-ui/core';
-import {AccountCircle} from '@material-ui/icons';
+import { Button } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
 import React from 'react';
-
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 function RegistrationPage() {
     const goBack = () => {
         window.history.back();
     }
-    
+    const registerUser = async (e) => {
+        e.preventDefault();
+        let { user_name, email, user_password } = e.target;
+        let user = {
+            name: user_name.value,
+            email: email.value,
+            user_password: user_password.value
+        }
+        try {
+            const response = await axios.post('http://localhost:5000/register', user)
+            if (response.status === 200) {
+                window.alert('User added Successfully');
+                document.querySelector('.reg__page__form__loginNavigator').click();
+            }
+        }
+        catch (err) {
+            if (err.message.indexOf('422') !== -1) {
+                window.alert('Invalid Input')
+            }
+        }
+
+    }
     return (
         <div className="reg">
             <div className="reg__page">
                 <AccountCircle className="reg__page__avatar" />
-                <form className="reg__page__form">
-                    <input className="reg__page__form__input" type="text" placeholder="Enter the Name" />
-                    <input className="reg__page__form__input" type="text" placeholder="Enter the Email" />
-                    <input className="reg__page__form__input" type="password" placeholder="Type the Password" />
-                    <input className="reg__page__form__input" type="password" placeholder="Retype Password to Confirm" />
+                <form onSubmit={registerUser} className="reg__page__form">
+                    <input name="user_name" className="reg__page__form__input" type="text" placeholder="Enter the Name" />
+                    <input name="email" className="reg__page__form__input" type="text" placeholder="Enter the Email" />
+                    <input name="user_password" className="reg__page__form__input" type="password" placeholder="Type the Password" />
+                    <input name="confirm_password" className="reg__page__form__input" type="password" placeholder="Retype Password to Confirm" />
 
                     <div className="reg__page__form__buttons">
                         <Button className="reg__page__form__buttons__button" onClick={goBack}>Back</Button>
-                        <Button className="reg__page__form__buttons__button">Register</Button>
+                        <Button type="submit" className="reg__page__form__buttons__button">Register</Button>
                     </div>
                 </form>
+                <Link to="/login" className="reg__page__form__loginNavigator" ></Link>
             </div>
         </div>
     )
