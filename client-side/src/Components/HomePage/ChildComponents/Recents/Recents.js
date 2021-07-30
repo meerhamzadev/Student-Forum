1bee8c3d920337e1fecba2a6d5ebdd876502b0d7
MeerHamza1421 <hamzababar37@gmail.com>
2021-07-30 10:08:55 -0700

    📦 NEW: Add updatePassword funtionality

--- client-side/src/Components/HomePage/ChildComponents/Recents/Recents.js
+++ client-side/src/Components/HomePage/ChildComponents/Recents/Recents.js
@@ -3,16 +3,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Recents() {
+    const linkTo = (link) => {
+        if (!window.location.href.includes(link)) {
+            window.open(link, "_self");
+        }
+    }
     return (
         <div className="recents">
-            <h1 className="recents__heading">Recents</h1>
+            <h1 className="recents__heading">Actions</h1>
 
             <div className="recents__items">
                 <Button variant="extendedFab" className="recents__items__item" onClick={() => document.querySelector('.questionLink').click()} >Ask A Question</Button>
-                <Button variant="extendedFab" className="recents__items__item" onClick={() => document.querySelector('.questionLink').click()} >Give Answer</Button>
-                <Button variant="extendedFab" className="recents__items__item">Lorem Ipsum</Button>
-                <Button variant="extendedFab" className="recents__items__item">Lorem Ipsum</Button>
+                <Button variant="extendedFab" className="recents__items__item" onClick={() => linkTo("/changePassword")} >Update Password</Button>
+                <Button variant="extendedFab" className="recents__items__item" onClick={() => linkTo("/deleteUser")}>Delete Account</Button>
+                <Button variant="extendedFab" className="recents__items__item" onClick={() => document.querySelector('.mainpageLink').click()}>Logout</Button>
                 <Link to="/questions" className="questionLink" ></Link>
+                <Link to="/" className="mainpageLink"></Link>
             </div>
         </div>
     )
