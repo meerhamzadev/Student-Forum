a85f99765e608d279072cb11046b6abc3687a255
meerhamzadev <hamzababar37@gmail.com>
2022-08-04 16:41:20 +0500

    📦 NEW: Configure Cloud Mysql

--- backend/database-connection/db-connection.js
+++ backend/database-connection/db-connection.js
@@ -1,16 +1,20 @@
-import {createConnection} from 'mysql';
-
-const connection = createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: 'student-forum',
-    port: 3306,
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+const connection = mysql.createConnection({
+    host: "bgb3sdef1pqpmuucvfht-mysql.services.clever-cloud.com",
+    user: "u8ohlzg55k8kj5ls",
+    password: "Lw2piebEspQesieTRyPi",
+    database: "bgb3sdef1pqpmuucvfht",
+    port: "3306",
 });
 
 connection.connect(err => {
     if (err)
-        throw err;
+    {
+        console.log(err.message);
+        return;
+    }
 
     console.log("Database Connection established successfully......");
 });
